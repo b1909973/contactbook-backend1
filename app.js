3f2df59cbd0703fd2431f29804ddd15cf4052553
Nguyen Van Phuong <phuongb909973@student.ctu.edu.vn>
2022-09-16 02:15:06 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 //         exports:{}
 
 // }
-
+const contactsRouter = require("./app/routes/contact.route");
 express = require('express')
 const cors = require('cors')
 const app = express();
@@ -11,12 +11,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/contacts',contactsRouter)
+
 app.get("/",function(req,res){
 
-    res.json({message:"Welcome to contact-book application"});
+    res.json({message:"Welcome to contact-book application 1"});
 })
 
-  
 module.exports= app
 
 // return module.exports
