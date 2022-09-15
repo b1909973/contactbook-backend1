3fef4a00a4b3117851d031576ca5e9d4c47b6426
Nguyen Van Phuong <phuongb909973@student.ctu.edu.vn>
2022-09-16 01:43:42 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,22 @@
+// var module = {
+//         exports:{}
+
+// }
+
+express = require('express')
+const cors = require('cors')
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",function(req,res){
+
+    res.json({message:"Welcome to contact-book application"});
+})
+
+  
+module.exports= app
+
+// return module.exports
